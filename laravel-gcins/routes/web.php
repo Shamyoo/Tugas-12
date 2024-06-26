0c584aa814877d92e113813f25eeb7d2875e9272
shamm <auzoranov18@gmail.com>
2024-06-27 00:04:00 +0700

    Mengubah file tugas 12 menjadi tugas 13

--- laravel-gcins/routes/web.php
+++ laravel-gcins/routes/web.php
@@ -8,23 +8,15 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-// use ;
-// use ;
+Route::get('/', function () {
+    return view('table');
+});
 
-// Route::get('/', 'HomeController@index');
-Route::get('/', 'App\Http\Controllers\HomeController@index');
-
-// Route Registrasi
-Route::get('/register', 'App\Http\Controllers\AuthController@register');
-Route::post('/register', 'App\Http\Controllers\AuthController@store');
-
-// Route Welcome
-// Route::get('/welcome', function(){
-//     return view('welcome');
-// });
-
+Route::get('/dataTable', function (){
+    return view('dataTable');
+});
